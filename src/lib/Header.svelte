8284f51f15dddb896055527d3e67f3fc41c83bca
TylorJB <tylorbennett18@gmail.com>
2023-08-31 19:01:22 +1200

    Added grids and 2 photos, still a work in progress

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -8,7 +8,7 @@
     </div>
     <title> Pretty Nuts</title>
     <div class= 'navbar'>       
-        <img src="/img/Springbok Dollaz-logos_transparent.png"
+        <img src="/img/SoScary-logos_transparent.png"
         width="200"
         height="200"/>    
         <Nav/>
@@ -16,16 +16,15 @@
     
 </header>
 
+<div class="container">
+    <div class="item item-1">1</div>
+    <div class="item item-2">2</div>
+    <div class="item item-3">3</div>
+    <div class="item item-4">4</div>
+</div>
+
 
 <div class="page1">
 <div class="top"></div>
 <div class="bottom"></div>
 </div>
-
-
-<style>
-    .navbar{
-        display: flex;
-        align-items: center;               
-    }   
-</style>
