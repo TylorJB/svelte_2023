7dbe4c1f70f6f088588d8ae02a4d39c8886b2505
TylorJB <tylorbennett18@gmail.com>
2023-11-07 17:14:21 +1300

    Ive added a background class as well as the actual background, with styling. Ive also imported a google font for the text on my second page

--- src/routes/more-info/+page.svelte
+++ src/routes/more-info/+page.svelte
@@ -1,7 +1,21 @@
-<div class="more-info-container">
-    <div class="more-info-container2">           
-        <div class="more-info-logo">
-            <img src="/img/SoScary-logos_transparent.png" alt="logo" class="more-info-image">
+<div class="more-info-background">
+    <div class="more-info-container">
+        <div class="more-info-container2">           
+            <div class="more-info-logo">
+                <img src="/img/SoScary-logos_transparent.png" alt="logo" class="more-info-image">
+            </div>
+            <div class="more-info-text">
+                <p>As you venture further into this eerie cyber-domain, you will encounter spine-tingling tales, nightmarish visuals, and ghastly sounds that will awaken the deepest fears that lurk within you. Dare you continue, or will you succumb to the terror that awaits? Brace yourself, for you are about to enter a world where fear reigns supreme, and your very senses will be put to the test. Welcome to the website that exists to scare you to your core.</p>
+            </div>
         </div>
     </div>
 </div>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
+
+    .more-info-background{
+    background-image: url("/img/freestocks-kVv8AvoKJ7g-unsplash (1).jpg");
+    background-size:80%;
+    }
+</style>
