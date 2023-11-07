ab25e74bfd8893cefd5fc6b1eb10cf595b451284
TylorJB <tylorbennett18@gmail.com>
2023-11-07 17:07:05 +1300

    Ive added two containers and an image to the more-info page

--- src/routes/more-info/+page.svelte
+++ src/routes/more-info/+page.svelte
@@ -1,1 +1,7 @@
-
+<div class="more-info-container">
+    <div class="more-info-container2">           
+        <div class="more-info-logo">
+            <img src="/img/SoScary-logos_transparent.png" alt="logo" class="more-info-image">
+        </div>
+    </div>
+</div>
