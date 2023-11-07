46a030adea85d6fe697f3f95e3d5a5690732eae7
TylorJB <tylorbennett18@gmail.com>
2023-11-07 17:40:38 +1300

    Saving changes to my routes folder

--- src/routes/more-info/+page.svelte
+++ src/routes/more-info/+page.svelte
@@ -1,3 +1,8 @@
+<svelte:head>
+    <link rel="stylesheet" href="css/main.css" />
+</svelte:head>
+
+
 <div class="more-info-background">
     <div class="more-info-container">
         <div class="more-info-container2">           
